a1963eed927343d3d743067458b1a4101ff9ceb3
Joshua Retallick <josh@old-empire.co.uk>
2020-12-01 16:25:30 +0000

    Updated thermostat function with PSM

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,20 +4,38 @@ class Thermostat{
   constructor() {
     this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
+    this.powerSavingMode = true;
   }
+
   getCurrentTemperature() {
     return this.temperature;
   }
+
   up() {
     this.temperature += 1
   }
+
   down() {
     if (this.isMinimumTemperature()) {
       return;
     }
     this.temperature -= 1
   }
+
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
   }
+
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+
+  switchPowerSavingModeOff() {
+  this.powerSavingMode = false;
+  }
+
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+
 };
